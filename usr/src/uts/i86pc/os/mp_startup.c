797d060fee1298e4f360788600c82dcff867d8cd
Toomas Soome <tsoome@me.com>
2017-05-21 20:08:36 +0300

    i86pc/os/mp_startup.c

--- usr/src/uts/i86pc/os/mp_startup.c
+++ usr/src/uts/i86pc/os/mp_startup.c
@@ -539,7 +539,7 @@ mp_cpu_unconfigure_common(struct cpu *cp, int error)
 		trap_trace_ctl_t *ttc = &trap_trace_ctl[cp->cpu_id];
 
 		kmem_free((void *)ttc->ttc_first, trap_trace_bufsize);
-		ttc->ttc_first = NULL;
+		ttc->ttc_first = 0;
 	}
 #endif
 
