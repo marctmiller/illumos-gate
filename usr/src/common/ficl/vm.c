81c9ab4d6e5b50d5c015ba9fa32de86ade9e8cb4
Toomas Soome <tsoome@me.com>
2017-07-01 10:43:17 +0300

    ficl: variable 'count' might be clobbered by 'longjmp' or 'vfork'
    /code/illumos-gate/usr/src/common/ficl/vm.c:255:6: error: variable 'count' might be clobbered by 'longjmp' or 'vfork' [-Werror=clobbered]
      int count;
          ^~~~~
    cc1: all warnings being treated as errors

--- usr/src/common/ficl/vm.c
+++ usr/src/common/ficl/vm.c
@@ -252,7 +252,7 @@ ficlVmInnerLoop(ficlVm *vm, ficlWord *fw)
 	jmp_buf exceptionHandler;
 	int except;
 	int once;
-	int count;
+	volatile int count;
 	ficlInstruction instruction;
 	ficlInteger i;
 	ficlUnsigned u;
